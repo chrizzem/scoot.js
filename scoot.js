a1a56318cb8b2850b87bb2f3acaaed0c7c87dea2
chrizzem <chris.medinger@gmail.com>
2013-09-11 17:10:06 -0700

    Update scoot.js
    objectified for more flexible use of the parts

--- scoot.js
+++ scoot.js
@@ -1,32 +1,40 @@
-  /*yes, yes, it requires jQuery. */
-  /*fades images under .noscoot in after load*/
-  $('li.noscoot>div:last-child>img').load(function(){
-	  var image=$(this);
-	  var imageDiv=image.parent();
-	  if(imageDiv.is(':first-child')){ //don nothing... the non-glamorous point about is()
-	  }else{
-	  var hoverDiv=imageDiv.parent().children('div:first-child');
-	  var setHeight = { "line-height": theHeight,
-	  		 "height": theHeight
-	  		 };
-	  hoverDiv.css ( setHeight );
-	  }
-	  imageDiv.parents('.noscoot').animate({opacity:1},300);
-	});
-       
-    /*fades in images under .scoot after load and then slides them to reveal the text underneath */   
-  $('li.scoot>div:last-child>img').load(function(){
-	  var image=$(this);
-	  var imageDiv=image.parent();
-	  var linkDiv=imageDiv.parent().children('div:nth-last-child(2)');
-	  var hoverDiv=imageDiv.parent().children('div:first-child');
-	  var theHeight = image.css('height'); 
-	  var setHeight = { "line-height": theHeight,
-	  		 "height": theHeight
-	  		 };
-	  var delayTime=(Math.random()*900+100); // gives the slide a staggered effect. Change the multiplier to change the variability.
-	  hoverDiv.css( setHeight ); // this could be expanded with a conditional to check if hoverDiv and linkDiv are the same Div, in which case only set one.
-	  linkDiv.css( setHeight );
-	  imageDiv.parents('.scoot').animate({opacity:1},300);
-	  imageDiv.delay(delayTime).animate({ left: image.css('width')}, 600);
-	});
+  /*Dependencies: jQuery 
+  *Explanation: fades images under .noscoot in after load
+  *Example: see the example index.html at https://github.com/chrizzem/scoot.js as an example.
+  */
+  
+ /*sets top and bottom padding of an element based on the size of a reference element. */ 
+ 
+function setPad(reference, target){
+    var padAmt=((reference.height() - target.height())/2);
+    var styles = {
+      "padding-top": padAmt,
+      "padding-bottom": padAmt
+      };
+    target.css( styles );
+};
+ 
+ /* scootSize: resizes sibling divs to the image size so that text, etc. is centered. It's using padding 
+ *so that you can overlay other elements and hide them with background effects.
+ */
+function scootSize(image){
+    var imageDiv=image.parent();
+    if(imageDiv.is(':first-child')){  
+        return (0);//do nothing... the unglamorous thing about is() is no isnot
+    }else{
+        var hoverDiv = imageDiv.parent().children('.hover');
+        var linkDiv = imageDiv.parent().children('.link');
+        setPad(image, hoverDiv);
+        setPad(image, linkDiv);
+    }
+};
+
+ /* scootIt: scoots the image aside (or at least the div it is in) to reveal the sibling div(s) below.
+  * the image should be in a div that is the last div child of a list item.
+ */
+function scootIt(image){
+    var delayTime=(Math.random()*1000+300); // gives the slide a staggered effect. Change the multiplier to change the variability.
+    image.parent().delay(delayTime).animate({ left: image.css('width')}, 600, function(){
+      image.css('opacity', 0); //animate:complete
+    });
+};
